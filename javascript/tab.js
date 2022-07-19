4a25d175de6103688bfce31f9bf83e1ec9ad713b
french13 <skswnddl6@gmail.com>
2022-07-19 23:54:11 +0900

    level2-2추가

---
+++ javascript/tab.js
@@ -0,0 +1,30 @@
+
+document.querySelectorAll('.tab-button')[0].addEventListener('click', function () {
+    document.querySelectorAll('.tab-button')[0].classList.add('orange');
+    document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[2].classList.remove('orange');
+
+    document.querySelectorAll('.tab-content')[0].classList.add('show');
+    document.querySelectorAll('.tab-content')[1].classList.remove('show');
+    document.querySelectorAll('.tab-content')[2].classList.remove('show');
+})
+
+document.querySelectorAll('.tab-button')[1].addEventListener('click', function () {
+    document.querySelectorAll('.tab-button')[0].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[1].classList.add('orange');
+    document.querySelectorAll('.tab-button')[2].classList.remove('orange');
+
+    document.querySelectorAll('.tab-content')[0].classList.remove('show');
+    document.querySelectorAll('.tab-content')[1].classList.add('show');
+    document.querySelectorAll('.tab-content')[2].classList.remove('show');
+})
+
+document.querySelectorAll('.tab-button')[2].addEventListener('click', function () {
+    document.querySelectorAll('.tab-button')[0].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+    document.querySelectorAll('.tab-button')[2].classList.add('orange');
+
+    document.querySelectorAll('.tab-content')[0].classList.remove('show');
+    document.querySelectorAll('.tab-content')[1].classList.remove('show');
+    document.querySelectorAll('.tab-content')[2].classList.add('show');
+})
