253f903f6e5a537a6e4d11039d262b45e4f49daa
french13 <skswnddl6@gmail.com>
2022-09-19 13:43:20 +0900

    react

---
+++ react/blog/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let post = '강남 우동 맛집'
+
+  let [a, b]  = useState('남자 코트 추천')
+
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h4>{a}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
