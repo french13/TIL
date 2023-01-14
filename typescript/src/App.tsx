fa56876ffcc7e247bb159ea1c315026cb241373e
french13 <skswnddl6@gmail.com>
2023-01-14 17:00:17 +0900

    타입스크립트 연습

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,25 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
-
-  let 이름 :string[] = ['kim', 'park']
-
-  function test(x : number) : number{
-    return x+2
-  }
-  
-type Member ={
-  name : string,
-  age : number
-}
-
-const john:Member = {
-  name : 'kim',
-  age : 12
-}
-
 
   return (
     <div className="App">
